6c69b8081c98c637226236fc0bcdb187561232ca
heysivani <heysivani@gmail.com>
2020-10-19 15:52:58 +0900

    fixed link b/w app.js and server; basic webpage now works

---
+++ client/app.js
@@ -0,0 +1,5 @@
+const body = document.body;
+
+const test = document.createElement("div");
+test.innerText = "I'M ALIVE";
+body.append(test);
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+
 const app = express();
 const port = 7777;
 
-app.get("./", (req, res) => {
-    res.send("i'm working");
-})
+app.use(express.static("./client"));
+
+//check
+// app.get("/check", (req, res) => {
+//     res.send("i'm working");
+// })
 
 app.listen(port, () => {
     console.log(`we hear u @${port}`);
